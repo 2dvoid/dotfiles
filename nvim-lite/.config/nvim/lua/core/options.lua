1b573ef79ab8ee0e0f3cda9f4ac9310933b6345f
2dvoid <2dvoid@mail.com>
2026-07-03 22:03:16 +0600

    Nvim-lite: Line number configuration

---
+++ nvim-lite/.config/nvim/lua/core/options.lua
@@ -0,0 +1,15 @@
+-- Line Numbering
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Number column modification
+vim.opt.statuscolumn = " %s%l    "
+
+-- Enable current line number highlighting
+-- vim.opt.cursorline = true
+-- vim.opt.cursorlineopt = "number"
+-- Color
+-- vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#fbbf24", bold = true })
+
+
+
