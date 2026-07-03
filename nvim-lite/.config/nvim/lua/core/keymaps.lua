40df3662dd3df1f9178be65ca64b0e33d94f17ef
2dvoid <2dvoid@mail.com>
2026-07-03 17:51:38 +0600

    Nvim-lite: Added keybind jk to Escape

--- nvim-lite/.config/nvim/lua/core/keymaps.lua
+++ nvim-lite/.config/nvim/lua/core/keymaps.lua
@@ -1,3 +1,6 @@
 -- Leader Key (Try to keep this setting around the top of this file)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+-- Pressing jk simulates Escape in insert mode
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
