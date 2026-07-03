87cbacd45b73be698ba88510b784bd2161a883d4
2dvoid <2dvoid@mail.com>
2026-07-03 20:04:18 +0600

    Nvim-lite: Added no-neck-pain plugin for centering layout

---
+++ nvim-lite/.config/nvim/lua/plugins/no-neck-pain.lua
@@ -0,0 +1,14 @@
+return {
+  -- Source
+  "shortcuts/no-neck-pain.nvim",
+  version = "*",
+  
+  -- Settings
+  opts = {
+    width = 90, 
+    autocmds = {
+      -- Auto startup
+      enableOnVimEnter = true, 
+    },
+  }
+}
