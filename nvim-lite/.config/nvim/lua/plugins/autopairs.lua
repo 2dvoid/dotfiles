2c30fb1c3212f54f2a226e2abb3c71381973c853
2dvoid <2dvoid@mail.com>
2026-07-03 17:38:13 +0600

    Nvim-lite: Added autopair plugin

---
+++ nvim-lite/.config/nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,18 @@
+return {
+  -- Source
+  "echasnovski/mini.pairs",
+
+  -- Triggering event
+  event = "InsertEnter",
+
+  -- Options / Settings
+  opts = {} 
+
+  -- Power Switch
+  -- [Won't be needed here. If "opts" is provided, this Power Switch is never needed. It is automatically called by the Lazy package manager.]
+  --
+  -- config = function()
+  --  require("mini.pairs").setup({})
+  -- end
+}
+
