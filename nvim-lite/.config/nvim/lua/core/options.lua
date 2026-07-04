999857e685fbb0e88c5f8546c94ca2df11e6f6e1
2dvoid <2dvoid@mail.com>
2026-07-04 15:33:13 +0600

    Nvim-lite: Fixed Tab space behaviour

--- nvim-lite/.config/nvim/lua/core/options.lua
+++ nvim-lite/.config/nvim/lua/core/options.lua
@@ -36,3 +36,11 @@ vim.on_key(function(char)
         end
     end
 end, vim.api.nvim_create_namespace("auto_hlsearch"))
+
+
+-- Fix Tab space behaviour
+vim.opt.tabstop = 4        -- A physical TAB character is rendered as 4 spaces wide
+vim.opt.shiftwidth = 4     -- Pressing >> or << shifts the code by exactly 4 spaces
+vim.opt.expandtab = true   -- Converts TAB presses into physical spaces (Absolute strict requirement for Python)
+vim.opt.smartindent = true -- Intuitively indents the next line when you press Enter after a { or a loop
+
